83361ef946ccae01681fcdba56656199964e8ea3
Costeer <costeer@protonmail.com>
2024-09-27 16:30:21 +0200

    Fixed logo

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "101st Wiki",
+    pageTitle: "LOL",
     enableSPA: true,
     enablePopovers: false,
     analytics: {
